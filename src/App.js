483e3ec75f45e7c70c6cce81f5aa7e682d6b0c7e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-23 14:30:10 -0700

    save progress on the form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
       loading: false,
       activity: {}
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -17,11 +18,18 @@ class App extends React.Component {
       .then(data => 
         this.setState({
           loading: false,
-          activity: data
+          activity: data,
+          
           })
         )
   }
 
+  handleChange(event) {
+    const {name, value} = event.target; //for some reason getting an error in console about uncontrolled state
+    this.setState({
+      [name]: value
+    })
+  }
   
 
   render() {
@@ -29,6 +37,25 @@ class App extends React.Component {
     return (
       <div className="App">
       <p>{text}</p> 
+      <input
+      type="text"
+      value={this.state.firstName}
+      name="firstName"
+      placeholder="first name"
+      onChange={this.handleChange}
+      >
+      </input>
+
+      <input
+      type="text"
+      value={this.state.lastName}
+      name="lastName"
+      placeholder="last name"
+      onChange={this.handleChange}
+      >
+      </input>
+      <h1>{this.state.firstName} {this.state.lastName}</h1>
+
       </div>
     );
   }
